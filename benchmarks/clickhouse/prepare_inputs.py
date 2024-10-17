3b42fb627e9d17da7bf1c6971192cc3e79fbc7f4
YifanYang6 <yifanyang6@link.cuhk.edu.cn>
2024-10-17 17:29:00 +0800

    benchmarks/clickhouse/prepare_inputs.py: switch to clickhouse_connect to resolve JSON parsing issue
    Signed-off-by: YifanYang6 <yifanyang6@link.cuhk.edu.cn>

--- benchmarks/clickhouse/prepare_inputs.py
+++ benchmarks/clickhouse/prepare_inputs.py
@@ -1,104 +1,71 @@
-from clickhouse_driver import Client
-import pandas as pd
-import time
 import os
+import time
+
+import clickhouse_connect
+import pandas as pd
+from clickhouse_connect.driver.client import Client
 
 namespace = "ts-dev"
 
 
 def generate_metric(start_time, end_time) -> pd.DataFrame:
     # 连接到 ClickHouse 客户端
-    client = Client("10.26.1.146", user="default", password="nn", database="default")
 
+
+    client = clickhouse_connect.get_client(host="10.26.1.146", username="default", password="nn")
     # 定义查询语句
-    query = """
+    query = f"""
 SELECT 
     TimeUnix,
     MetricName, 
+    MetricDescription, 
     Value, 
     ServiceName,
     MetricUnit,
-    ResourceAttributes,
-    Attributes
+    toJSONString(ResourceAttributes) AS ResourceAttributes,
+    toJSONString(Attributes) AS Attributes
 FROM 
-    otel_metrics_gauge
+    otel_metrics_gauge om
 WHERE 
-    (ResourceAttributes['k8s.namespace.name'] = %(namespace)s
-     OR NOT mapContains(ResourceAttributes, 'k8s.namespace.name'))
-    AND TimeUnix BETWEEN %(start_time)s AND %(end_time)s
+    om.ResourceAttributes['k8s.namespace.name'] = '{namespace}'
+    AND om.TimeUnix BETWEEN '{start_time}' AND '{end_time}'
     """
 
-    # 设置查询参数
-    params = {"namespace": namespace, "start_time": start_time, "end_time": end_time}
+    result = client.raw_query(query=query, fmt="CSVWithNames")
 
-    # 执行查询
-    result = client.execute(query, params)
-
-    # 定义 DataFrame 的列名
-    selected_columns = [
-        "TimeUnix",
-        "MetricName",
-        "Value",
-        "ServiceName",
-        "MetricUnit",
-        "ResourceAttributes",
-        "Attributes",
-    ]
-
-    # 将查询结果转换为 pandas DataFrame
-    df = pd.DataFrame(result, columns=selected_columns)
-
-    return df
+    return result
 
 
 def generate_metric_sum(start_time, end_time) -> pd.DataFrame:
     # 连接到 ClickHouse 客户端
-    client = Client("10.26.1.146", user="default", password="nn", database="default")
+    client = clickhouse_connect.get_client(host="10.26.1.146", username="default", password="nn")
 
     # 定义查询语句
     query = f"""
     SELECT 
         TimeUnix,
         MetricName, 
+        MetricDescription, 
         Value, 
         ServiceName,
         MetricUnit,
-        ResourceAttributes,
-        Attributes
+        toJSONString(ResourceAttributes) AS ResourceAttributes,
+        toJSONString(Attributes) AS Attributes
     FROM 
-        otel_metrics_sum
+        otel_metrics_sum omg
     WHERE 
-        (ResourceAttributes['k8s.namespace.name'] = %(namespace)s
-        OR NOT mapContains(ResourceAttributes, 'k8s.namespace.name'))
-        AND TimeUnix BETWEEN %(start_time)s AND %(end_time)s
+        omg.ResourceAttributes['k8s.namespace.name'] = '{namespace}'
+        AND omg.TimeUnix BETWEEN '{start_time}' AND '{end_time}'
     """
 
-    # 设置查询参数
-    params = {"namespace": namespace, "start_time": start_time, "end_time": end_time}
+    result = client.raw_query(query=query, fmt="CSVWithNames")
 
-    # 执行查询
-    result = client.execute(query, params)
-
-    # 定义 DataFrame 的列名
-    selected_columns = [
-        "TimeUnix",
-        "MetricName",
-        "Value",
-        "ServiceName",
-        "MetricUnit",
-        "ResourceAttributes",
-        "Attributes",
-    ]
-
-    # 将查询结果转换为 pandas DataFrame
-    df = pd.DataFrame(result, columns=selected_columns)
-
-    return df
+    return result
 
 
 def generate_metric_histogram(start_time, end_time) -> pd.DataFrame:
     # 连接到 ClickHouse 客户端
-    client = Client("10.26.1.146", user="default", password="nn", database="default")
+    client = clickhouse_connect.get_client(host="10.26.1.146", username="default", password="nn")
 
     # 定义查询语句
     query = f"""
@@ -107,8 +74,8 @@ def generate_metric_histogram(start_time, end_time) -> pd.DataFrame:
         MetricName, 
         ServiceName,
         MetricUnit,
-        ResourceAttributes,
-        Attributes,
+        toJSONString(ResourceAttributes) AS ResourceAttributes,
+        toJSONString(Attributes) AS Attributes,
         Count,
         Sum,
         BucketCounts,
@@ -117,45 +84,21 @@ def generate_metric_histogram(start_time, end_time) -> pd.DataFrame:
         Max,
         AggregationTemporality
     FROM 
-        otel_metrics_histogram
+        otel_metrics_histogram omh
     WHERE 
-        (ResourceAttributes['k8s.namespace.name'] = %(namespace)s
-        OR NOT mapContains(ResourceAttributes, 'k8s.namespace.name'))
-        AND TimeUnix BETWEEN %(start_time)s AND %(end_time)s
+        omh.ResourceAttributes['k8s.namespace.name'] = '{namespace}'
+        AND omh.TimeUnix BETWEEN '{start_time}' AND '{end_time}'
     """
 
-    # 设置查询参数
-    params = {"namespace": namespace, "start_time": start_time, "end_time": end_time}
-
     # 执行查询
-    result = client.execute(query, params)
+    result = client.raw_query(query=query, fmt="CSVWithNames")
 
-    # 定义 DataFrame 的列名
-    selected_columns = [
-        "TimeUnix",
-        "MetricName",
-        "ServiceName",
-        "MetricUnit",
-        "ResourceAttributes",
-        "Attributes",
-        "Count",
-        "Sum",
-        "BucketCounts",
-        "ExplicitBounds",
-        "Min",
-        "Max",
-        "AggregationTemporality",
-    ]
-
-    # 将查询结果转换为 pandas DataFrame
-    df = pd.DataFrame(result, columns=selected_columns)
-
-    return df
+    return result
 
 
 def generate_log(start_time, end_time) -> pd.DataFrame:
     # 连接到 ClickHouse 客户端
-    client = Client("10.26.1.146", user="default", password="nn", database="default")
+    client = clickhouse_connect.get_client(host="10.26.1.146", username="default", password="nn")
 
     # 定义查询语句
     query = f"""
@@ -168,45 +111,24 @@ def generate_log(start_time, end_time) -> pd.DataFrame:
         SeverityNumber,
         ServiceName,
         Body,
-        ResourceAttributes,
+        toJSONString(ResourceAttributes) AS ResourceAttributes,
         LogAttributes
     FROM 
-        otel_logs
+        otel_logs ol
     WHERE 
-        (ResourceAttributes['k8s.namespace.name'] = %(namespace)s
-        OR NOT mapContains(ResourceAttributes, 'k8s.namespace.name'))
-        AND Timestamp BETWEEN %(start_time)s AND %(end_time)s
+        ol.ResourceAttributes['service.namespace'] = '{namespace}'
+        AND ol.Timestamp BETWEEN '{start_time}' AND '{end_time}'
     """
 
-    # 设置查询参数
-    params = {"namespace": namespace, "start_time": start_time, "end_time": end_time}
-
     # 执行查询
-    result = client.execute(query, params)
+    result = client.raw_query(query=query, fmt="CSVWithNames")
 
-    # 定义 DataFrame 的列名
-    selected_columns = [
-        "Timestamp",
-        "TimestampTime",
-        "TraceId",
-        "SpanId",
-        "SeverityText",
-        "SeverityNumber",
-        "ServiceName",
-        "Body",
-        "ResourceAttributes",
-        "LogAttributes",
-    ]
-
-    # 将查询结果转换为 pandas DataFrame
-    df = pd.DataFrame(result, columns=selected_columns)
-
-    return df
+    return result
 
 
 def generate_trace(start_time, end_time) -> pd.DataFrame:
     # 连接到 ClickHouse 客户端
-    client = Client("10.26.1.146", user="default", password="nn", database="default")
+    client = clickhouse_connect.get_client(host="10.26.1.146", username="default", password="nn")
 
     # 定义查询语句
     query = f"""
@@ -218,50 +140,26 @@ def generate_trace(start_time, end_time) -> pd.DataFrame:
         SpanName,
         SpanKind,
         ServiceName,
-        toJSONString(ResourceAttributes) AS ResourceAttributes_JSON,
+        toJSONString(ResourceAttributes) AS ResourceAttributes,
         SpanAttributes,
         Duration,
         StatusCode,
         StatusMessage
     FROM 
-        otel_traces
+        otel_traces ot
     WHERE 
-        (ResourceAttributes['service.namespace'] = %(namespace)s)
-        AND Timestamp BETWEEN %(start_time)s AND %(end_time)s
+        ot.ResourceAttributes['service.namespace'] = '{namespace}'
+        AND ot.Timestamp BETWEEN '{start_time}' AND '{end_time}'
     """
-
-    # 设置查询参数
-    params = {"namespace": namespace, "start_time": start_time, "end_time": end_time}
-
     # 执行查询
-    result = client.execute(query, params)
+    result = client.raw_query(query=query, fmt="CSVWithNames")
 
-    # 定义 DataFrame 的列名
-    selected_columns = [
-        "Timestamp",
-        "TraceId",
-        "SpanId",
-        "ParentSpanId",
-        "TraceState",
-        "SpanName",
-        "SpanKind",
-        "ServiceName",
-        "ResourceAttributes",
-        "SpanAttributes",
-        "Duration",
-        "StatusCode",
-        "StatusMessage",
-    ]
-
-    # 将查询结果转换为 pandas DataFrame
-    df = pd.DataFrame(result, columns=selected_columns)
-
-    return df
+    return result
 
 
 def generate_trace_id_ts(start_time, end_time) -> pd.DataFrame:
     # 连接到 ClickHouse 客户端
-    client = Client("10.26.1.146", user="default", password="nn", database="default")
+    client = clickhouse_connect.get_client(host="10.26.1.146", username="default", password="nn")
 
     # 定义查询语句
     query = f"""
@@ -271,23 +169,14 @@ def generate_trace_id_ts(start_time, end_time) -> pd.DataFrame:
     FROM 
         otel_traces_trace_id_ts
     WHERE 
-        Start BETWEEN %(start_time)s AND %(end_time)s
-        AND End BETWEEN %(start_time)s AND %(end_time)s
+        Start BETWEEN '{start_time}' AND '{end_time}'
+        AND End BETWEEN '{start_time}' AND '{end_time}'
     """
 
-    # 设置查询参数
-    params = {"namespace": namespace, "start_time": start_time, "end_time": end_time}
-
     # 执行查询
-    result = client.execute(query, params)
+    result = client.raw_query(query=query, fmt="CSVWithNames")
 
-    # 定义 DataFrame 的列名
-    selected_columns = ["TraceId", "Start", "End"]
-
-    # 将查询结果转换为 pandas DataFrame
-    df = pd.DataFrame(result, columns=selected_columns)
-
-    return df
+    return result
 
 
 def generate_data_nezha(start_time, end_time) -> pd.DataFrame:
@@ -320,39 +209,26 @@ def generate_data_nezha(start_time, end_time) -> pd.DataFrame:
     # 将查询结果转换为 pandas DataFrame
     df = pd.DataFrame(result, columns=selected_columns)
 
-    df_pivot = df.pivot_table(
-        index=["TimeStamp", "PodName"], columns="MetricName", values="Value"
-    ).reset_index()
+    df_pivot = df.pivot_table(index=["TimeStamp", "PodName"], columns="MetricName", values="Value").reset_index()
 
     return df_pivot
 
 
-def save_to_csv(df: pd.DataFrame, filename: str):
-    """
-    将 DataFrame 保存为 CSV 文件。
-
-    :param df: 要保存的 pandas DataFrame
-    :param filename: 保存的文件名（包括路径）
-    """
-    try:
-        df.to_csv(filename, index=False, encoding="utf-8-sig")
-        print(f"数据已成功保存到 {filename}")
-    except Exception as e:
-        print(f"保存 CSV 文件时出错: {e}")
-
+def save_to_csv(result: bytes, filename: str):
+    with open(filename, "w") as f:
+        f.write(result.decode("utf-8"))
+    print(f"数据已成功保存到 {filename}")
 
 if __name__ == "__main__":
     # 获取当前时间和开始时间（过去 10 分钟）
     end_time = int(time.time())
     start_time = end_time - 600
-
+    print(start_time, end_time)
     os.mkdir("input")
 
     save_to_csv(generate_metric(start_time, end_time), "input/metrics.csv")
     save_to_csv(generate_metric_sum(start_time, end_time), "input/metric_sum.csv")
-    save_to_csv(
-        generate_metric_histogram(start_time, end_time), "input/metrics_histogram.csv"
-    )
+    save_to_csv(generate_metric_histogram(start_time, end_time), "input/metrics_histogram.csv")
     save_to_csv(generate_log(start_time, end_time), "input/logs.csv")
     save_to_csv(generate_trace(start_time, end_time), "input/traces.csv")
     save_to_csv(generate_trace_id_ts(start_time, end_time), "input/trace_id_ts.csv")
