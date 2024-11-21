8494ceecda92149f875a75809ce51abaad044839
lincyaw <814750204@qq.com>
2024-11-21 11:42:42 +0800

    format python code

--- algorithms/dummyalgo/rca.py
+++ algorithms/dummyalgo/rca.py
@@ -2,13 +2,14 @@ from typing import Dict
 from pprint import pprint
 import os
 
+
 def print_directory_tree(start_path, prefix=""):
     # 获取当前目录下的所有文件和子目录
     items = os.listdir(start_path)
     items.sort()  # 可选：按字母排序
     for i, item in enumerate(items):
         # 确定是否是最后一个元素，用于绘制正确的符号
-        is_last = (i == len(items) - 1)
+        is_last = i == len(items) - 1
         # 拼接路径
         path = os.path.join(start_path, item)
         # 根据是否是最后一个元素选择符号
@@ -23,13 +24,13 @@ def print_directory_tree(start_path, prefix=""):
 # IMPORTANT: do not change the function signature!!
 def start_rca(params: Dict):
     pprint(params)
-    directory = '/app/output'
+    directory = "/app/output"
     print_directory_tree("./")
 
     if not os.path.exists(directory):
         os.makedirs(directory)
 
-    file_path = os.path.join(directory, 'my_file.txt')
+    file_path = os.path.join(directory, "my_file.txt")
 
-    with open(file_path, 'w') as file:
-        file.write('hello world jj')
+    with open(file_path, "w") as file:
+        file.write("hello world jj")
