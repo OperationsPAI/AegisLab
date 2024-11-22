9c10df31671f76b79ce5165d18bbe39c1e5ea6a1
YifanYang6 <yifanyang6@link.cuhk.edu.cn>
2024-11-22 17:04:48 +0800

    algorithms/metis: use prebuild image
    Signed-off-by: YifanYang6 <yifanyang6@link.cuhk.edu.cn>

--- algorithms/metis/RCA/AD/abstract/__init__.py
+++
@@ -1,3 +0,0 @@
-from .detector import Detector
-from .parser import Parser
-from .calculator import Calculator
--- algorithms/metis/RCA/AD/abstract/calculator.py
+++
@@ -1,7 +0,0 @@
-from abc import ABC, abstractmethod
-
-
-class Calculator(ABC):
-    @staticmethod
-    @abstractmethod
-    async def calculate(self, data, normal): ...
