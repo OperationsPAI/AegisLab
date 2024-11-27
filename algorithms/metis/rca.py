9962a2fef12902feb14555112ef4a58bf1fde915
YifanYang6 <yifanyang6@link.cuhk.edu.cn>
2024-11-27 15:31:48 +0800

    algorithms/metis/rca.py: Reorganize and standardize output
    Signed-off-by: YifanYang6 <yifanyang6@link.cuhk.edu.cn>

--- algorithms/metis/rca.py
+++ algorithms/metis/rca.py
@@ -6,6 +6,7 @@ import shutil
 from functools import partial
 from pathlib import Path
 from typing import Dict, List, Tuple
+
 import pandas as pd
 from aiomultiprocess import Pool
 from evaluation import evaluate
@@ -82,17 +83,12 @@ async def concat(results):
     return await asyncio.to_thread(pd.concat, results)
 
 
-async def filter_csv_data(
-    query_type, start_time, end_time, input_file_paths, pool: Pool
-):
+async def filter_csv_data(query_type, start_time, end_time, input_file_paths, pool: Pool):
     """Filter CSV data for logs, metrics, and traces based on the query type."""
     batch_size = 10000
     if query_type == "log":
         log_df = pd.read_csv(input_file_paths["log_file"], parse_dates=["Timestamp"])
-        batches = [
-            log_df.iloc[start : start + batch_size].copy()
-            for start in range(0, len(log_df), batch_size)
-        ]
+        batches = [log_df.iloc[start : start + batch_size].copy() for start in range(0, len(log_df), batch_size)]
         results = await pool.starmap(
             parselog,
             zip(batches, [start_time] * len(batches), [end_time] * len(batches)),
@@ -101,16 +97,11 @@ async def filter_csv_data(
         return filtered_df
 
     elif query_type == "metric":
-        df_gauge = pd.read_csv(
-            input_file_paths["metric_file"], parse_dates=["TimeUnix"]
-        )
-        df_sum = pd.read_csv(
-            input_file_paths["metric_sum_file"], parse_dates=["TimeUnix"]
-        )
+        df_gauge = pd.read_csv(input_file_paths["metric_file"], parse_dates=["TimeUnix"])
+        df_sum = pd.read_csv(input_file_paths["metric_sum_file"], parse_dates=["TimeUnix"])
 
         gauge_batches = [
-            df_gauge.iloc[start : start + batch_size].copy()
-            for start in range(0, len(df_gauge), batch_size)
+            df_gauge.iloc[start : start + batch_size].copy() for start in range(0, len(df_gauge), batch_size)
         ]
         gauge_results = await pool.starmap(
             parsemetric,
@@ -122,10 +113,7 @@ async def filter_csv_data(
         )
         filtered_gauge = await pool.apply(concat, (gauge_results,))
 
-        sum_batches = [
-            df_sum.iloc[start : start + batch_size].copy()
-            for start in range(0, len(df_sum), batch_size)
-        ]
+        sum_batches = [df_sum.iloc[start : start + batch_size].copy() for start in range(0, len(df_sum), batch_size)]
         sum_results = await pool.starmap(
             parsemetric,
             zip(
@@ -136,26 +124,16 @@ async def filter_csv_data(
         )
         filtered_sum = await pool.apply(concat, (sum_results,))
 
-        filtered_sum = filtered_sum[
-            (
-                filtered_sum["MetricName"].isin(
-                    ["k8s.pod.network.io", "k8s.pod.network.errors"]
-                )
-            )
-        ]
+        filtered_sum = filtered_sum[(filtered_sum["MetricName"].isin(["k8s.pod.network.io", "k8s.pod.network.errors"]))]
 
         # Combine the results
         filtered_df = pd.concat([filtered_gauge, filtered_sum])
         return filtered_df
 
     elif query_type == "trace":
-        trace_df = pd.read_csv(
-            input_file_paths["trace_file"], parse_dates=["Timestamp"]
-        )
+        trace_df = pd.read_csv(input_file_paths["trace_file"], parse_dates=["Timestamp"])
         # Apply filters
-        filtered_df = trace_df[
-            (trace_df["Timestamp"] >= start_time) & (trace_df["Timestamp"] <= end_time)
-        ]
+        filtered_df = trace_df[(trace_df["Timestamp"] >= start_time) & (trace_df["Timestamp"] <= end_time)]
         parent_df = trace_df[["SpanId", "ServiceName"]].rename(
             columns={"SpanId": "ParentSpanId", "ServiceName": "ParentServiceName"}
         )
@@ -176,14 +154,10 @@ async def filter_csv_data(
         raise ValueError("Invalid query type")
 
 
-async def collect_and_save_data(
-    folder, start_time, end_time, data_type, input_file_paths, pool
-):
+async def collect_and_save_data(folder, start_time, end_time, data_type, input_file_paths, pool):
     """Collect and save data in batches."""
     filepath = Path(folder) / f"{data_type}s.csv"
-    filtered_df = await filter_csv_data(
-        data_type, start_time, end_time, input_file_paths, pool
-    )
+    filtered_df = await filter_csv_data(data_type, start_time, end_time, input_file_paths, pool)
     await asyncio.to_thread(filtered_df.to_csv, filepath, index=False, mode="w")
 
 
@@ -216,9 +190,7 @@ async def process_case(normal_range, abnormal_range, input_file_paths, pool):
     print(f"Processing abnormal range: {abnormal_start} - {abnormal_end}")
     normal_folder, abnormal_folder = create_folders()
     tasks = [
-        collect_and_save_data(
-            folder, start_time, end_time, data_type, input_file_paths, pool
-        )
+        collect_and_save_data(folder, start_time, end_time, data_type, input_file_paths, pool)
         for folder, start_time, end_time in [
             (normal_folder, normal_start, normal_end),
             (abnormal_folder, abnormal_start, abnormal_end),
@@ -250,9 +222,7 @@ async def start_rca(params: Dict):
     childconcurrency = 20
     processes = os.cpu_count()
     queuecount = processes // 4
-    async with Pool(
-        processes=processes, childconcurrency=childconcurrency, queuecount=queuecount
-    ) as pool:
+    async with Pool(processes=processes, childconcurrency=childconcurrency, queuecount=queuecount) as pool:
         await process_case(
             normal_range=normal_time_range,
             abnormal_range=abnormal_time_range,
@@ -263,7 +233,11 @@ async def start_rca(params: Dict):
     result = Path("data") / "final_ranking.csv"
     Path("/app/output").mkdir(exist_ok=1, parents=1)
     if result.exists():
-        shutil.copy(result, "/app/output/result.csv")
+        df = pd.read_csv(result)
+        df.rename(columns={"CombinedScore": "confidence", "ServiceName": "result", "Index": "rank"}, inplace=True)
+        df["level"] = "service"
+        df = df[["level", "result", "rank", "confidence"]]
+        df.to_csv("/app/output/result.csv", index=False)
 
 
 def print_directory_tree(start_path, prefix=""):
