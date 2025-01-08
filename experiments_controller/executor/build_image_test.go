dc4d4a16c50b4d086882a40acc42648b6b252039
lincyaw <814750204@qq.com>
2025-01-08 20:20:33 +0800

    add buildkit

--- experiments_controller/executor/build_image_test.go
+++ experiments_controller/executor/build_image_test.go
@@ -9,9 +9,9 @@ import (
 func TestBuildImage(t *testing.T) {
 	err := buildDockerfileAndPush(context.Background(), BuildOptions{
 		DockerfilePath: "/home/nn/workspace/rcabench/algorithms/detector/builder.Dockerfile", // todo: remove the absolute path
-		ImageName:      "10.10.10.240/library/detector11:latest",
+		ImageName:      "10.10.10.240/library/detecto1:latest12",
 		BuildArgs:      map[string]string{},
-		ContextDir:     ".",
+		ContextDir:     "/home/nn/workspace/rcabench/algorithms/detector",
 		Target:         "",
 	})
 	if err != nil {
