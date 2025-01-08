d83f2239a089568d97e23503dab5d93db8670558
lincyaw <814750204@qq.com>
2025-01-08 21:03:52 +0800

    fix stuck issue

--- experiments_controller/executor/build_image_test.go
+++ experiments_controller/executor/build_image_test.go
@@ -8,8 +8,8 @@ import (
 
 func TestBuildImage(t *testing.T) {
 	err := buildDockerfileAndPush(context.Background(), BuildOptions{
-		DockerfilePath: "/home/nn/workspace/rcabench/algorithms/detector/builder.Dockerfile", // todo: remove the absolute path
-		ImageName:      "10.10.10.240/library/detecto1:latest12",
+		DockerfilePath: "/home/nn/workspace/rcabench/algorithms/e-diagnose/builder.Dockerfile", // todo: remove the absolute path
+		ImageName:      "10.10.10.240/library/e-diagnose:latest12",
 		BuildArgs:      map[string]string{},
 		ContextDir:     "/home/nn/workspace/rcabench/algorithms/detector",
 		Target:         "",
