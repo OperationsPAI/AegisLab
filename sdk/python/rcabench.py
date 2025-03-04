eb9464fea882d69856ff20adfa9d324fd1bc5074
rainystevn1 <rainystevn1@gmail.com>
2025-03-04 23:18:51 +0800

    refactor rcabench sdk

--- sdk/python/rcabench.py
+++ sdk/python/rcabench.py
@@ -1,13 +1,13 @@
+from typing import Any, List, Dict, Optional
+from dataclasses import dataclass
 import inspect
 import requests
-from dataclasses import dataclass
-from typing import Any, List, Dict, Optional
 
 
 @dataclass
 class TaskResponse:
-    taskID: str
-    message: str
+    group_id: str
+    task_ids: List[str]
 
 
 @dataclass
@@ -26,9 +26,13 @@ class TaskDetails:
 
 
 @dataclass
-class AlgoBenchResponse:
-    benchmarks: List[str]
+class AlgorithmResp:
     algorithms: List[str]
+
+
+@dataclass
+class EvaluationResp:
+    results: List
 
 
 @dataclass
@@ -59,14 +63,89 @@ class RunAlgorithmPayload:
     dataset: str
 
 
-class RCABenchSDK:
-    URL_DICT = {
-        "get_algorithms": "{}/algorithms",
-        "inject": "{}/injections",
-        "get_injection_parameters": "{}/injections/parameters",
-        "get_injection_namespace_pod_info": "{}/injections/namespace_pods",
+class BaseRouter:
+    URL_PREFIX = ""
+
+    def __init__(self, sdk):
+        self.sdk = sdk
+
+    def _build_url(self, endpoint: str) -> str:
+        return f"{self.URL_PREFIX}{endpoint}"
+
+
+class Algorithm(BaseRouter):
+    URL_PREFIX = "/algorithms"
+
+    URL_ENDPOINTS = {
+        "execute": "",
+        "list": "/list",
+        "get_stream": "/{task_id}/stream",
     }
 
+    def execute(self, payload: List[Dict]) -> TaskResponse:
+        url = self._build_url(self.URL_ENDPOINTS["execute"])
+        return self.sdk._post(url, payload)["data"]
+
+    def list(self) -> AlgorithmResp:
+        """
+        Retrieve available benchmarks and algorithms.
+        """
+        url = self._build_url(self.URL_ENDPOINTS["list"])
+        data = self.sdk._get(url)["data"]
+        return AlgorithmResp(algorithms=data["algorithms"])
+
+    def get_stream(self, task_id: str) -> List[str]:
+        endpoint = self.URL_ENDPOINTS["get_stream"].format(task_id=task_id)
+        url = self._build_url(endpoint)
+        return self.sdk._process_stream(url)
+
+
+class Evaluation(BaseRouter):
+    URL_PREFIX = "/evaluations"
+
+    URL_ENDPOINTS = {
+        "execute": "",
+    }
+
+    def execute(self, payload: Dict) -> EvaluationResp:
+        url = self._build_url(self.URL_ENDPOINTS["execute"])
+        data = self.sdk._get(url, params=payload)["data"]
+        return EvaluationResp(results=data["results"])
+
+
+class Injection(BaseRouter):
+    URL_PREFIX = "/injections"
+
+    URL_ENDPOINTS = {
+        "execute": "",
+        "get_namespace_pod_info": "/namespace_pods",
+        "get_parameters": "/parameters",
+        "get_stream": "/{task_id}/stream",
+    }
+
+    def execute(self, payload: List[Dict]) -> TaskResponse:
+        url = self.URL_ENDPOINTS["execute"]
+        return self.sdk._post(url, payload)["data"]
+
+    def get_namespace_pod_info(self) -> NamespacePodInfo:
+        url = self.URL_ENDPOINTS["get_namespace_pod_info"]
+        data = self.sdk._get(url)["data"]
+        return NamespacePodInfo(namespace_info=data["namespace_info"])
+
+    def get_parameters(self) -> InjectionParameters:
+        url = self.URL_ENDPOINTS["get_parameters"]
+        data = self.sdk._get(url)["data"]
+        return InjectionParameters(
+            specification=data["specification"], keymap=data["keymap"]
+        )
+
+    def get_stream(self, task_id: str) -> List[str]:
+        endpoint = self.URL_ENDPOINTS["get_stream"].format(task_id=task_id)
+        url = self._build_url(endpoint)
+        return self.sdk._process_stream(url, task_id)
+
+
+class RCABenchSDK:
     def __init__(self, base_url: str):
         """
         Initialize the SDK with the base URL of the server.
@@ -74,6 +153,9 @@ class RCABenchSDK:
         :param base_url: Base URL of the RCABench server, e.g., "http://localhost:8080"
         """
         self.base_url = base_url.rstrip("/") + "/api/v1"
+        self.algorithm = Algorithm(self)
+        self.evaluation = Evaluation(self)
+        self.injection = Injection(self)
 
     @staticmethod
     def _get_parent_function_name():
@@ -81,37 +163,40 @@ class RCABenchSDK:
         parent_frame = stack[2]
         return parent_frame.function
 
-    def _get(self, task_id: Optional[str] = None) -> Any:
-        url = self.URL_DICT[self._get_parent_function_name()].format(self.base_url)
-        if task_id:
-            url = url.format(task_id)
-        response = requests.get(url)
+    def _get(
+        self, url: str, params: Optional[Dict] = None, stream: bool = False
+    ) -> Any:
+        url = f"{self.base_url}{url}"
+        if not stream:
+            response = requests.get(url, params=params)
+            response.raise_for_status()
+            return response.json()
+        return requests.get(url, params=params, stream=True)
+
+    def _post(self, url: str, payload: List[Dict]) -> requests.Response:
+        url = f"{self.base_url}{url}"
+        response = requests.post(url, json=payload)
         response.raise_for_status()
         return response.json()
 
-    def _post(self, payload: List[Dict]) -> requests.Response:
-        url = self.URL_DICT[self._get_parent_function_name()].format(self.base_url)
-        json_payload = [item for item in payload]
-        return requests.post(url, json=json_payload)
+    def _process_stream(self, url: str) -> List[str]:
+        response = self._get(url, stream=True)
+        lines = []
 
-    def get_algorithms(self) -> AlgoBenchResponse:
-        """
-        Retrieve available benchmarks and algorithms.
+        try:
+            # 持续读取事件流
+            for line in response.iter_lines():
+                if line:
+                    # 解码字节流（过滤心跳包的空行）
+                    decoded_line = line.decode("utf-8")
+                    print(decoded_line)
+                    lines.append(decoded_line)
 
-        :return: AlgoBenchResponse object
-        """
-        data = self._get()["data"]
-        return AlgoBenchResponse(algorithms=data["algorithms"])
+        except KeyboardInterrupt:
+            print("Manual connection termination")
+        except Exception as e:
+            print(f"Connection error: {str(e)}")
+        finally:
+            response.close()
 
-    def inject(self, payload: List[Dict]) -> TaskResponse:
-        return self._post(payload).json()
-
-    def get_injection_parameters(self) -> InjectionParameters:
-        data = self._get()["data"]
-        return InjectionParameters(
-            specification=data["specification"], keymap=data["keymap"]
-        )
-
-    def get_injection_namespace_pod_info(self) -> NamespacePodInfo:
-        data = self._get()["data"]
-        return NamespacePodInfo(namespace_info=data["namespace_info"])
+        return lines
--- sdk/python/test_rcabench.py
+++ sdk/python/test_rcabench.py
@@ -1,7 +1,9 @@
-import unittest
+from typing import Dict, List, Optional
 from rcabench import RCABenchSDK, RunAlgorithmPayload
 from pprint import pprint
+import json
 import random
+import unittest
 
 
 class InjectHelper:
@@ -42,17 +44,40 @@ class InjectHelper:
 
 
 class TestRCABenchSDK(unittest.TestCase):
-    def __init__(self, methodName="runTest"):
+    def __init__(self, url: str, methodName="runTest"):
         super().__init__(methodName)
         # 替换为实际服务器地址
-        self.base_url = "http://10.10.10.220:32080"
+        self.base_url = url
         self.sdk = RCABenchSDK(self.base_url)
 
     def test_get_algorithms(self):
-        print(self.sdk.get_algorithms())
+        pprint(self.sdk.get_algorithms())
 
-    def test_submit_injection(self):
-        n_trail = 10
+    def test_get_evaluations(self):
+        base_url = "http://localhost:8082"
+        sdk = RCABenchSDK(base_url)
+        algo_bench = sdk.get_algo_bench()
+        pprint(algo_bench)
+        datasets = sdk.get_datasets()
+        pprint(datasets)
+        task_response = sdk.submit_task(
+            "RunAlgorithm",
+            RunAlgorithmPayload(
+                algorithm="metis",
+                benchmark=random.choice(algo_bench.benchmarks),
+                dataset=random.choice(datasets.datasets),
+            ),
+        )
+        pprint(task_response)
+
+        task_id = task_response.taskID
+        status_response = sdk.get_task_status(task_id)
+        pprint(status_response)
+
+        details = sdk.get_task_details(task_id)
+        pprint(details)
+
+    def test_submit_injection(self, n_trial: int = 10):
         excluded_pods = ["mysql"]
 
         injection_params = self.sdk.get_injection_parameters()
@@ -65,7 +90,7 @@ class TestRCABenchSDK(unittest.TestCase):
         pprint(namespace_pod_info)
 
         faults = []
-        for _ in range(n_trail):
+        for _ in range(n_trial):
             pod = random.choice(namespace_pod_info.namespace_info[namespace])
             params = helper.generate_injection_dict()
             while params["fault_type"] in excluded_pods:
@@ -84,40 +109,98 @@ class TestRCABenchSDK(unittest.TestCase):
 
         pprint(faults)
 
-        task_response = self.sdk.inject(faults)
+        task_response = self.sdk.submit_injection(faults)
         pprint(task_response)
 
-        # for task_id in task_response["data"]:
-        #     status_response = sdk.get_task_status(task_id)
-        #     pprint(status_response)
+        return task_response
 
-        #     details = sdk.get_task_details(task_id)
-        #     pprint(details)
+    @staticmethod
+    def _parse_json(message: str) -> Optional[str]:
+        lines = message.strip().split("\n")
 
-    def test_submit_evaluation(self):
-        base_url = "http://localhost:8082"
-        sdk = RCABenchSDK(base_url)
-        algo_bench = sdk.get_algo_bench()
-        pprint(algo_bench)
-        datasets = sdk.get_datasets()
-        pprint(datasets)
-        task_response = sdk.submit_task(
-            "RunAlgorithm",
-            RunAlgorithmPayload(
-                algorithm="metis",  # random.choice(algo_bench.algorithms)
-                benchmark=random.choice(algo_bench.benchmarks),
-                dataset=random.choice(datasets.datasets),
-            ),
+        data_parts = []
+        for line in lines:
+            data_part = line[len("data:") :].strip()
+            data_parts.append(data_part)
+
+        combined_data = "".join(data_parts)
+
+        result_dict = None
+        try:
+            result_dict = json.loads(combined_data)
+        except json.JSONDecodeError as e:
+            print(f"Failed to parse json: {e}")
+
+        return result_dict
+
+    def _get_stream_results(self, response: Dict, stream_func, key: str) -> List[str]:
+        results = []
+        for task_id in response["task_ids"]:
+            lines = stream_func(task_id)
+            for line in lines:
+                if line.startswith("data"):
+                    result_dict = self._parse_json(line)
+                    value = result_dict.get(key, None)
+                    if value:
+                        results.append(value)
+
+        return results
+
+    def test_injection_and_building(self) -> List[str]:
+        injection_payload = [
+            {
+                "duration": 1,
+                "faultType": 5,
+                "injectNamespace": "ts",
+                "injectPod": "ts-preserve-service",
+                "spec": {"CPULoad": 1, "CPUWorker": 3},
+                "benchmark": "clickhouse",
+            }
+        ]
+
+        injection_resp = self.sdk.injection.submit_injection(injection_payload)
+        pprint(injection_resp)
+
+        datasets = self._get_results(
+            injection_resp, self.sdk.get_injection_stream, "dataset"
         )
-        pprint(task_response)
+        pprint(datasets)
 
-        task_id = task_response.taskID
-        status_response = sdk.get_task_status(task_id)
-        pprint(status_response)
+        return datasets
 
-        details = sdk.get_task_details(task_id)
-        pprint(details)
+    def test_algorithm_and_collection(
+        self, algorithms: List[str], datasets: List[str]
+    ) -> List[str]:
+        algorithm_payload = []
+        for dataset in datasets:
+            for algorithm in algorithms:
+                algorithm_payload.extend(
+                    [
+                        {
+                            "benchmark": "clickhouse",
+                            "algorithm": algorithm,
+                            "dataset": dataset,
+                        },
+                    ]
+                )
+
+        algorithm_resp = self.sdk.algorithm.execute(algorithm_payload)
+        pprint(algorithm_resp)
+
+        execution_ids = self._get_stream_results(
+            algorithm_resp, self.sdk.algorithm.get_stream, "execution_id"
+        )
+        pprint(execution_ids)
+
+        return execution_ids
+
+    def test_workflow(self, algorithms: List[str]) -> List[str]:
+        pass
 
 
 if __name__ == "__main__":
-    TestRCABenchSDK().test_submit_injection()
+    url = "http://localhost:8082"
+    TestRCABenchSDK(url).test_algorithm_and_collection(
+        algorithms=["e-diagnose"],
+        datasets=["ts-ts-preserve-service-cpu-exhaustion-7wqhd5"],
+    )
