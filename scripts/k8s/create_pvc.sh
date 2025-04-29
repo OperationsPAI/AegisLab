517d169d07b2d3a4d77efe897ff964454cf99a09
lincyaw <814750204@qq.com>
2025-04-29 18:34:45 +0800

    clean up the pvc

--- scripts/k8s/create_pvc.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Default namespace if not provided
-NAMESPACE=${1:-experiment}
-
-kubectl create ns $NAMESPACE
-kubectl apply -f algorithm_pv.yaml -n $NAMESPACE
-kubectl apply -f dataset_pv.yaml -n $NAMESPACE
-kubectl create secret generic kube-config --from-file=config=/home/nn/.kube/config -n $NAMESPACE
